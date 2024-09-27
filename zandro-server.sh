10809e77529a2a41f63d9fc729b640a8a26aeac4
kultasakaali <kultasakaali@gmail.com>
2024-09-27 22:35:32 +0300

    Bump mod file versions, update maplists, move music back to optional

--- zandro-server.sh
+++ zandro-server.sh
@@ -23,26 +23,26 @@ qZandronumPath="/home/kulta/q-zandronum/q-zandronum-server"
 wads_load_always="qcde_pvpvisibility.pk3"
 wads_optional="qcde--frankfurtloadingscreen.pk3"
 
-qcde="qcdev3.0_beta_23.pk3"
-qcdemaps="qcdemaps3.0_beta_23.pk3"
+qcde="qcdev3.0_beta_31.pk3"
+qcdemaps="qcdemaps3.0_beta_30.pk3"
 communitymaps="qcde_communitymaps_v18.pk3"
-qcdemus="qcdemus3.0_beta_2.pk3"
+qcdemus="qcdemus3.0_beta_31.pk3"
 hdfaces="qcde--hdfaces3.0_beta_8.pk3"
 voxels="qcde--voxels3.0_beta_20.pk3"
-announcer="georgeexleyannouncer.pk3"
+announcer=""
 
-utweapons="qcde_ut_weapons_v3.0_beta_23.pk3"
+utweapons="qcde_ut_weapons_v3.0_beta_31.pk3"
 utweapons_hires="qcde_ut_weapons_v3.0_hires_beta_20.pk3"
-utmovement="qcde_allutmov_v3.0_beta_20.pk3"
+utmovement="qcde_ut_movement_v3.0_beta_27.pk3"
 utvoxels="qcde_ut_weapons_v3.0_voxels_beta_20.pk3"
-utmus="qcdemus_ut_v3.0_beta_2.pk3"
-utannouncer="utannouncer_0.9.pk3"
+utmus="qcdemus_ut_v3.0_beta_31.pk3"
+utannouncer=""
 
 pve_maps_folder="/home/kulta/.config/zandronum/pvemaps"
 pve_monster_folder="/home/kulta/.config/zandronum/pvemons"
 
-maplist="QCDE01;QCDE02;QCDE04;QCDE05;QCDE06;QCDE07;QCDE08;QCDE09;QCDE10;QCDE11;QCDE13;QCDE14;QCDE15;QCDE16;QCDE17;QCDE18;QCDE19;QCDE20;QCDE21;QCDE22;QCDE24;QCDE25;QCDE26;QCDE27;QCDE28;QCDE29;QCDE30;QCDE31;QCDE32;QCDE33;QCDE34;QCDE35;QCDE36;QCDE37;QCDE38;QCDE39;QCDE40;QCDE41;QCDE42;QCDE43;QCDE44;QCDE45;QCDE46;QCDE47;QCDE48;QCDE49;QCDE50;QCDL02;QCDL03;QCDL04;QCDL05;QCDL06;QCDL07;QCDL08;QCDL09;QCDL10;QCME01;QCME02"
-duellist="QCDE01;QCDE02;QCDE05;QCDE06;QCDE08;QCDE09;QCDE10;QCDE11;QCDE14;QCDE15;QCDE16;QCDE18;QCDE19;QCDE20;QCDE21;QCDE22;QCDE24;QCDE26;QCDE27;QCDE28;QCDE29;QCDE30;QCDE32;QCDE35;QCDE37;QCDE39;QCDE40;QCDE41;QCDE42;QCDE45;QCDE46;QCDE48;QCDE50;QCDL02;QCDL03;QCDL04;QCDL05;QCDL06;QCDL07;QCDL09;QCDL10"
+maplist="QCDE01;QCDE02;QCDE04;QCDE05;QCDE06;QCDE07;QCDE08;QCDE09;QCDE10;QCDE11;QCDE13;QCDE14;QCDE15;QCDE16;QCDE17;QCDE18;QCDE19;QCDE20;QCDE21;QCDE22;QCDE23;QCDE24;QCDE25;QCDE26;QCDE27;QCDE28;QCDE29;QCDE30;QCDE31;QCDE32;QCDE33;QCDE34;QCDE35;QCDE36;QCDE37;QCDE38;QCDE39;QCDE40;QCDE41;QCDE42;QCDE43;QCDE44;QCDE45;QCDE46;QCDE47;QCDE48;QCDE49;QCDE50;QCDL02;QCDL03;QCDL04;QCDL05;QCDL06;QCDL07;QCDL08;QCDL09;QCDL10;QCME01;QCME02"
+duellist="QCDE01;QCDE02;QCDE05;QCDE06;QCDE08;QCDE09;QCDE10;QCDE11;QCDE14;QCDE15;QCDE16;QCDE18;QCDE19;QCDE20;QCDE21;QCDE22;QCDE23;QCDE24;QCDE26;QCDE27;QCDE28;QCDE29;QCDE30;QCDE32;QCDE35;QCDE37;QCDE39;QCDE40;QCDE41;QCDE42;QCDE45;QCDE46;QCDE48;QCDE50;QCDL02;QCDL03;QCDL04;QCDL05;QCDL06;QCDL07;QCDL09;QCDL10"
 communitylist=";QCCM03"
 communityduellist=""
 aeonlist=";AEON01;AEON02;AEON03;AEON04;AEON05;AEON06;AEON07;AEON08;AEON09;AEON10;AEON11;AEON12;AEON13;AEON14;AEON15;AEON16;AEON17;AEON18;AEON19;AEON20;AEON21;AEON22;AEON23;AEON24;AEON25;AEON26;AEON27;AEON28;AEON29;AEON30;AEON31;AEON32;AEON33"
@@ -277,8 +277,8 @@ fi
 case $engine in
     "Q-Zandronum")
         server_executable="$qZandronumPath"
-        qcdemaps+=" $communitymaps"
-        maplist+="$communitylist"
+#        qcdemaps+=" $communitymaps"
+#        maplist+="$communitylist"
         ;;
 
     "Q-Zandronum 1.2")
@@ -496,7 +496,7 @@ fi
 
 if [ "$useNeon" == "true" ];
 then
-    qcdemaps+=" neonqcde3.0_beta_4.pk3"
+    qcdemaps+=" neonqcde3.0_beta_5.pk3"
     maplist+=$neonlist
 fi
 
@@ -520,7 +520,7 @@ then
     iwad="DOOM2.WAD"
 fi
 
-args="-port $port -iwad $iwad -file $mapsets $qcde $qcdemaps $qcdemus $wads_load_always -optfile $voxels $hdfaces $announcer $wads_optional -file $additional_wads $map_list +exec $config $additional_params $starting_map"
+args="-port $port -iwad $iwad -file $mapsets $qcde $qcdemaps $wads_load_always -optfile $qcdemus $announcer $wads_optional -file $additional_wads $map_list +exec $config $additional_params $starting_map"
 
 export LD_LIBRARY_PATH=$(dirname $server_executable)
 
